05b249d3d9a6f3e414f080924f03cc6a51dbe743
Haojian <salivasauce@gmail.com>
2026-02-19 18:27:46 +0800

    added card svg

---
+++ src/components/CardSVG/CardSVG.component.jsx
@@ -0,0 +1,13 @@
+'use client';
+import "./CardSVG.style.min.css";
+
+const CardSVG = () => {
+  return (
+    <div>
+      <span className="pcard-as"></span>
+    </div>
+  )
+
+}
+
+export default CardSVG;
